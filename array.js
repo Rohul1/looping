65492c862a7c803367f446c32ec1aab27b53285b
Rohul1 <rohul669319@gmail.com>
2023-09-25 23:09:55 +0600

    added more

--- array.js
+++ array.js
@@ -1,175 +1,196 @@
 
-3.
-
-let color = ["Blue", "white", "Black"]
-console.log(color);
-
-4.
-let colorIndex = color[2];
-console.log(colorIndex);
-
-5.
-color.push("Pink");
-let colorAdded = color.push("Pink");
-console.log(color);
-
-6.
-
-console.log(color.length);
-
-
-// 8. while loop
-/* < print 1 to 5 number > */
-var input = 1;
-while (input <= 5) {
-    console.log(input);
-    input++;
-}
-
-
-// Do while
-
-let i = 1;
-do {
-    console.log("Happiness doesn't suit me");
-
-    i++;
-} while (i <= 10)
-
-
-
-// for loop
-
-for (i = 0; i <= 5; i++) {
-    console.log("Happily alone");
-}
-
-
-
-
-//////////
-// Namota
-// 2.
-
-let result = 0;
-for (i = 1; i <= 10; i++) {
-    result = i * 2
-    console.log(result)
-}
-// // // <or>
-for (i = 0; i <= 20; i = i + 2) {
-    console.log(i)
-}
-
 // 3.
-let result2 = 0;
-for (i = 1; i <= 10; i++) {
-    result2 = i * 3;
-    console.log(result2)
-}
-// //  <or>
 
-for (i = 0; i <= 30; i = i + 3) {
-    console.log(i)
-}
+// let color = ["Blue", "white", "Black"]
+// console.log(color);
 
-// // 4.
+// 4.
+// let colorIndex = color[2];
+// console.log(colorIndex);
 
-let result3 = 0;
-for (i = 1; i <= 10; i++) {
-    result3 = i * 4;
-    console.log(result3)
-}
-// // // /* <or> */
+// 5.
+// color.push("Pink");
+// let colorAdded = color.push("Pink");
+// console.log(color);
 
-for (i = 0; i <= 40; i = i + 4) {
-    console.log(i)
-}
+// 6.
+
+// console.log(color.length);
 
 
-// // 5.
+// // 8. while loop
+// /* < print 1 to 5 number > */
+// var input = 1;
+// while (input <= 5) {
+//     console.log(input);
+//     input++;
+// }
 
-let result4 = 0;
-for (i = 1; i <= 10; i++) {
-    result4 = i * 5;
-    console.log(result4)
-}
 
-// /* <or> */
+// // Do while
 
-for (i = 0; i <= 50; i = i + 5) {
-    console.log(i)
-}
+// let i = 1;
+// do {
+//     console.log("Happiness doesn't suit me");
 
-// // 6.
+//     i++;
+// } while (i <= 10)
 
-let result5 = 0;
-for (i = 1; i <= 10; i++) {
-    result5 = i * 6;
-    console.log(result5)
-}
-// // /* <or > */
 
-for (i = 0; i <= 60; i = i + 6) {
-    console.log(i)
-}
 
-// // 7.
+// // for loop
 
-let result6 = 0;
-for (i = 1; i <= 10; i++) {
-    result6 = i * 7;
-    console.log(result6)
-}
+// for (i = 0; i <= 5; i++) {
+//     console.log("Happily alone");
+// }
 
+
+
+
+// //////////
+// // Namota
+// // 2.
+
+// let result = 0;
+// for (i = 1; i <= 10; i++) {
+//     result = i * 2
+//     console.log(result)
+// }
+// // // // <or>
+// for (i = 0; i <= 20; i = i + 2) {
+//     console.log(i)
+// }
+
+// // 3.
+// let result2 = 0;
+// for (i = 1; i <= 10; i++) {
+//     result2 = i * 3;
+//     console.log(result2)
+// }
+// // //  <or>
+
+// for (i = 0; i <= 30; i = i + 3) {
+//     console.log(i)
+// }
+
+// // // 4.
+
+// let result3 = 0;
+// for (i = 1; i <= 10; i++) {
+//     result3 = i * 4;
+//     console.log(result3)
+// }
+// // // // /* <or> */
+
+// for (i = 0; i <= 40; i = i + 4) {
+//     console.log(i)
+// }
+
+
+// // // 5.
+
+// let result4 = 0;
+// for (i = 1; i <= 10; i++) {
+//     result4 = i * 5;
+//     console.log(result4)
+// }
+
+// // /* <or> */
+
+// for (i = 0; i <= 50; i = i + 5) {
+//     console.log(i)
+// }
+
+// // // 6.
+
+// let result5 = 0;
+// for (i = 1; i <= 10; i++) {
+//     result5 = i * 6;
+//     console.log(result5)
+// }
 // // // /* <or > */
 
-for (i = 0; i <= 70; i = i + 7) {
-    console.log(i)
-}
+// for (i = 0; i <= 60; i = i + 6) {
+//     console.log(i)
+// }
 
-// // 8.
+// // // 7.
 
-let result7 = 0;
-for (i = 1; i <= 10; i++) {
-    result7 = i * 8;
-    console.log(result7)
-}
-
+// let result6 = 0;
+// for (i = 1; i <= 10; i++) {
+//     result6 = i * 7;
+//     console.log(result6)
+// }
 
 // // // // /* <or > */
 
-for (i = 0; i <= 80; i = i + 8) {
-    console.log(i)
-}
+// for (i = 0; i <= 70; i = i + 7) {
+//     console.log(i)
+// }
 
+// // // 8.
 
-// // 9.
+// let result7 = 0;
+// for (i = 1; i <= 10; i++) {
+//     result7 = i * 8;
+//     console.log(result7)
+// }
 
-let result8 = 0;
-for (i = 1; i <= 10; i++) {
-    result8 = i * 9;
-    console.log(result8)
-}
 
 // // // // // /* <or > */
 
-
-for (i = 0; i <= 90; i = i + 9) {
-    console.log(i)
-}
-
-// 10.
-
-let result9 = 0;
-for (i = 1; i <= 10; i++) {
-    result9 = i * 10;
-    console.log(result9)
-}
-
-// // /* <or > */
+// for (i = 0; i <= 80; i = i + 8) {
+//     console.log(i)
+// }
 
 
-for (i = 0; i <= 100; i = i + 10) {
-    console.log(i)
+// // // 9.
+
+// let result8 = 0;
+// for (i = 1; i <= 10; i++) {
+//     result8 = i * 9;
+//     console.log(result8)
+// }
+
+// // // // // // /* <or > */
+
+
+// for (i = 0; i <= 90; i = i + 9) {
+//     console.log(i)
+// }
+
+// // 10.
+
+// let result9 = 0;
+// for (i = 1; i <= 10; i++) {
+//     result9 = i * 10;
+//     console.log(result9)
+// }
+
+// // // /* <or > */
+
+
+// for (i = 0; i <= 100; i = i + 10) {
+//     console.log(i)
+// }
+
+
+
+// console.log("simple for loop")
+// for (i = 0; i < 7; i++) {
+//     console.log(i);
+// }
+
+
+
+// for (i = 0; i < 10; i += 2) {
+//     console.log(i);
+// }
+
+
+let numbers = [45, 50, 55, 60, 65, 70, 75]
+
+for (i = 0; i <= 7; i++) {
+    let number = numbers[i]
+    console.log(number);
 }
